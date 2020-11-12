931f6c4d921abfb6e9a032ff9205852c67590c59
Melissa Chiang <mchiang62@gmail.com>
2020-11-12 06:10:19 -0500

    finished first input

--- assets/form.js
+++ assets/form.js
@@ -1,31 +1,45 @@
-// const nameErrors = document.getElementsByClassName("nameError")
-
 $(document).ready(function () {
-    document.getElementById('myButton').onclick = function validateForm() {
 
+    document.getElementById('myButton').onclick = function validateForm(event) {
+        event.preventDefault();
         const x = document.forms["myForm"]["fname"].value;
-        if (x === "") {
+        if (x.length < 2) {
             showErrorMsg()
-            minLength()
+            $("#fnameError").html(
+                "Minimum characters 2"
+            );
+        } else if (x.length >= 2) {
+            showNoMsg()
+            $("#fnameError").empty()
+
         }
-       
+
     }
 
     const showErrorMsg = () => {
-        event.preventDefault();
+        // event.preventDefault();
         $(".errorMessage").html(
             "ERROR: Oops, please fix the field(s) below and resubmit when finished"
         );
     }
-
-    const minLength = () => {
-        event.preventDefault();
-        $(".nameError").html(
-            "Minimum characters 2"
+    const showNoMsg = () => {
+        // event.preventDefault();
+        $(".errorMessage").html(
+            ""
         );
+    }
+
+
+    function displayConfirmation() {
+        event.preventDefault();
+        $("form").empty();
+        $("form").html("<p>Thank you for signing up!</p>");
+        $("p").css({
+            "font-size": "20pt",
+            "font-weight": "700",
+            "margin-top": "50px"
+        });
 
     }
 
 });
-
-
